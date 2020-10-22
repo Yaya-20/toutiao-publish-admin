fdb397fe5bfe27f24fe11f39ed427b0a68268996
YaYa <2459032282@qq.com>
2020-10-22 09:16:00 +0800

    项目初始化,调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
+/**
+ * 项目的启动入口
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
 
 Vue.config.productionTip = false
-
+// 创建vue根实例
+// 把router/store配置到vue中
 new Vue({
   router,
   store,
